53bf57fb0032d82b3c2e4ef0b01d0ce8f49fc4cf
Sacha551 <sr.nico.67@gmail.com>
2025-10-17 20:39:07 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y cinnamon-desktop lightdm
+dnf5 install -y cinnamon-desktop
 
 # Use a COPR Example:
 #
@@ -21,5 +21,4 @@ dnf5 install -y cinnamon-desktop lightdm
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket && \
-systemctl enable lightdm
+systemctl enable podman.socket
